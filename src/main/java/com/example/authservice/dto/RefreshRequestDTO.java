cb8b090cd3b235e5f859d806596a0af6379c6d5c
dlstlr1707 <dlstlr1707@naver.com>
2025-06-01 00:32:02 +0900

    modify code

--- src/main/java/com/example/authservice/dto/RefreshRequestDTO.java
+++ src/main/java/com/example/authservice/dto/RefreshRequestDTO.java
@@ -1,8 +1,12 @@
 package com.example.authservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RefreshRequestDTO {
     private String refreshToken;
 }
--- src/main/java/com/example/authservice/dto/UpdateAddressRequestDTO.java
+++ src/main/java/com/example/authservice/dto/UpdateAddressRequestDTO.java
@@ -1,10 +1,14 @@
 package com.example.authservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateAddressRequestDTO {
     private int userUid;
     private String mainAddress;
